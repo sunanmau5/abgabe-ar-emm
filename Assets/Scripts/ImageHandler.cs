f75df553648aa4aa8abf619770db52ddec33648d
sunanmau5 <s0566144@htw-berlin.de>
2020-11-19 17:41:23 +0100

    Update project
    Added all component for this project

---
+++ Assets/Scripts/ImageHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageHandler : MonoBehaviour
+{
+    [SerializeField] ARTrackedImageManager _trackedImageManager;
+    [SerializeField] GameObject _content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!_trackedImageManager)
+        {
+            _trackedImageManager = GetComponent<ARTrackedImageManager>();
+        }    
+    }
+
+    void OnEnable() => _trackedImageManager.trackedImagesChanged += OnChanged;
+    void OnDisable() => _trackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            var minLocalScalar = Mathf.Min(trackedImage.size.x, trackedImage.size.y) / 2;
+            trackedImage.transform.localScale = new Vector3(minLocalScalar, minLocalScalar, minLocalScalar);
+            _content.transform.parent = trackedImage.transform;
+            _content.transform.localScale = Vector3.one;
+        }
+    }
+}
